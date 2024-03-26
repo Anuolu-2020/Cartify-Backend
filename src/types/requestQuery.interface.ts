2708caf9d2ba0ce2eaf32acd7200efddfd7cd400
Anuolu-2020 <Anuolu1645@gmail.com>
2024-03-26 14:26:19 +0100

    feat: add page and limit field to ReqQuery generic

--- src/types/requestQuery.interface.ts
+++ src/types/requestQuery.interface.ts
@@ -1,13 +1,15 @@
 import mongoose from "mongoose";
 
 type productParams = {
-  productId: { type: mongoose.Types.ObjectId };
+  productId: { type: mongoose.Types.ObjectId } | string;
   productIds: string[];
 };
 //type ResBody = object;
 //type ReqBody = object;
 type ReqQuery = {
   fields: string;
+  page: string;
+  limit: string;
 };
 
 export { productParams, ReqQuery };
