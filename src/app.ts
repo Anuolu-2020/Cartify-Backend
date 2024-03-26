6d73fa9d7da4522f2fca71a025f4f5ee1c173d91
Anuolu-2020 <Anuolu1645@gmail.com>
2024-03-26 16:22:26 +0100

    feat: increased rate limit window to 20 mins

--- src/app.ts
+++ src/app.ts
@@ -72,7 +72,7 @@ const redisStore = new RedisStore({
 
 //RATE LIMITER MIDDLEWARE CONFIGURATION
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
+  windowMs: 20 * 60 * 1000, // 15 minutes
   max: 50, // limit each IP to 50 requests per windowMs
   message: "Too many requests from this IP, please try again after 15 minutes",
   standardHeaders: true, // Standard headers
