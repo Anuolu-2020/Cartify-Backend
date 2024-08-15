da6d050e3b8b94138f07d6327a218e6d0b17bc4a
Anuolu-2020 <Anuolu1645@gmail.com>
2024-08-16 00:45:39 +0100

    feat: implemented verification and sigining of paseto token for authentication

---
+++ src/utils/pasetoToken.ts
@@ -0,0 +1,40 @@
+import { NextFunction } from "express";
+import { V4 } from "paseto";
+import { errorHandler } from "./error.handler.class";
+import { UserPayload } from "../types/user";
+
+const { sign, verify } = V4;
+
+export async function signToken(payload: any, next: NextFunction) {
+	try {
+		const token = await sign(
+			payload,
+			process.env.PASETO_PRIVATE_KEY as string,
+			{
+				expiresIn: "2 hours",
+			},
+		);
+
+		return token;
+	} catch (err) {
+		console.log(err);
+		return next(new errorHandler(500, "An Error Occurred"));
+	}
+}
+
+export async function verifyToken(
+	token: string,
+	next: NextFunction,
+): Promise<UserPayload | null> {
+	try {
+		const payload = (await verify(
+			token,
+			process.env.PASETO_PUBLIC_KEY as string,
+		)) as UserPayload;
+
+		return payload;
+	} catch (err) {
+		next(new errorHandler(401, "Token invalid or expired"));
+		return;
+	}
+}
