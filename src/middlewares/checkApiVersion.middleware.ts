52bfd81687b5762408d27b304e89e73394eeb26e
Anuolu-2020 <Anuolu1645@gmail.com>
2024-08-16 00:42:56 +0100

    chore: minor change

--- src/middlewares/checkApiVersion.middleware.ts
+++ src/middlewares/checkApiVersion.middleware.ts
@@ -1,15 +1,17 @@
 import { NextFunction, Request, Response } from "express";
 
-const checkApiVersion = (req: Request, res: Response, next: NextFunction) => {
-  const { version } = req.params;
+export const checkApiVersion = (
+	req: Request,
+	res: Response,
+	next: NextFunction,
+) => {
+	const { version } = req.params;
 
-  if (version !== "1") {
-    res.status(404).json({
-      message: `Undefined API version accessed.`,
-      success: false,
-    });
-  }
-  next();
+	if (version !== "1") {
+		res.status(404).json({
+			message: `Undefined API version accessed.`,
+			success: false,
+		});
+	}
+	next();
 };
-
-export = checkApiVersion;
