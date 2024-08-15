469f3e594ee0afd2bbb96a478653538ca0826045
Anuolu-2020 <Anuolu1645@gmail.com>
2024-08-16 00:43:22 +0100

    feat: reimplemented authentication middleware

--- src/middlewares/checkAuthentication.ts
+++ src/middlewares/checkAuthentication.ts
@@ -1,13 +1,35 @@
 import { Request, Response, NextFunction } from "express";
+import { verifyToken } from "../utils/pasetoToken";
+import { errorHandler } from "../utils/error.handler.class";
+import { UserPayload } from "../types/user";
 
-const isAuthenticated = (req: Request, res: Response, next: NextFunction) => {
-  if (!req.isAuthenticated()) {
-    return res
-      .status(401)
-      .json({ message: "Unauthorized request made to this endpoint." });
-  }
+export const isAuthenticated = async (
+	req: Request,
+	_: Response,
+	next: NextFunction,
+) => {
+	const header = req.headers["authorization"];
 
-  next();
+	if (!header) {
+		return next(
+			new errorHandler(401, "Unauthorized request made to this endpoint"),
+		);
+	}
+
+	const [authType, token] = header.split(" ");
+
+	if (authType != "Bearer" || !token) {
+		return next(new errorHandler(401, "Invalid authorization format"));
+	}
+
+	const payload = await verifyToken(token, next);
+
+	if (!payload) {
+		return;
+	}
+
+	req["user"] = payload as UserPayload;
+
+	next();
 };
-
-export = isAuthenticated;
+//
