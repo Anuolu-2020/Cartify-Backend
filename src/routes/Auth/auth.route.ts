ca51c4275180cbe28d452611a9c7a1ceeef39bb9
Anuolu-2020 <Anuolu1645@gmail.com>
2024-08-16 00:43:48 +0100

    feat: change to use only oauth

--- src/routes/Auth/auth.route.ts
+++ src/routes/Auth/auth.route.ts
@@ -2,30 +2,14 @@ import express, { Router } from "express";
 
 // Importing the controller functions
 import {
-  handlePasswordSignUp,
-  handlePasswordSignUpRedirect,
-  handlePasswordSignInRedirect,
-  handlePasswordSignIn,
-  handleSignOut,
-  handleGoogle,
-  handleGoogleRedirect,
+	handleSignOut,
+	handleGoogle,
 } from "../../controllers/Auth/auth.controller";
 
-const authRoute: Router = express.Router();
+export const authRoute: Router = express.Router();
 
 // HANDLE GOOGLE SIGN-UP/SIGN-IN REQUESTS
 authRoute.route("/google").post(handleGoogle);
-authRoute.route("/google/redirect/success").get(handleGoogleRedirect);
-
-// HANDLE SIGN-UP REQUESTS
-authRoute.route("/sign-up").post(handlePasswordSignUp);
-authRoute.route("/sign-up/redirect/success").get(handlePasswordSignUpRedirect);
-
-// HANDLE SIGN-IN REQUESTS
-authRoute.route("/sign-in").post(handlePasswordSignIn);
-authRoute.route("/sign-in/redirect/success").get(handlePasswordSignInRedirect);
 
 // HANDLE SIGN-OUT REQUEST
 authRoute.route("/sign-out").post(handleSignOut);
-
-export = authRoute;
