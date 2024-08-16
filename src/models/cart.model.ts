fa41f1b816fe2ea89de89a8bc0a415c4833f880a
Anuolu-2020 <Anuolu1645@gmail.com>
2024-08-17 00:17:36 +0100

    chore: changed userId to user

--- src/models/cart.model.ts
+++ src/models/cart.model.ts
@@ -4,36 +4,36 @@ import { ICart } from "./cart.interface";
 const Schema = mongoose.Schema;
 
 const cartSchema = new Schema<ICart>(
-  {
-    userId: {
-      type: mongoose.Types.ObjectId,
-      ref: "Users",
-    },
-    products: [
-      {
-        productId: { type: mongoose.Types.ObjectId, ref: "Products" },
-        quantity: {
-          type: Number,
-          required: [true, "Product quantity must be provided"],
-        },
-        name: {
-          type: String,
-          required: [true, "A product name must be provided"],
-        },
-        price: {
-          type: Number,
-          required: [true, "Product price is required"],
-          min: [0, "The minimum price shouldn't be below zero"],
-        },
-      },
-    ],
-    totalPrice: {
-      type: Number,
-      min: [0, "The minimum price shouldn't be below zero"],
-      required: true,
-    },
-  },
-  { timestamps: true },
+	{
+		user: {
+			type: mongoose.Types.ObjectId,
+			ref: "Users",
+		},
+		products: [
+			{
+				productId: { type: mongoose.Types.ObjectId, ref: "Products" },
+				quantity: {
+					type: Number,
+					required: [true, "Product quantity must be provided"],
+				},
+				name: {
+					type: String,
+					required: [true, "A product name must be provided"],
+				},
+				price: {
+					type: Number,
+					required: [true, "Product price is required"],
+					min: [0, "The minimum price shouldn't be below zero"],
+				},
+			},
+		],
+		totalPrice: {
+			type: Number,
+			min: [0, "The minimum price shouldn't be below zero"],
+			required: true,
+		},
+	},
+	{ timestamps: true },
 );
 
 const cartModel = mongoose.model<ICart>("Cart", cartSchema);
