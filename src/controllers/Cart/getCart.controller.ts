b89f3a330f8407cfe05bbb0ec6e80c25e1cc9ddb
Anuolu-2020 <Anuolu1645@gmail.com>
2024-08-17 00:10:38 +0100

    chore: minor change

--- src/controllers/Cart/getCart.controller.ts
+++ src/controllers/Cart/getCart.controller.ts
@@ -4,24 +4,24 @@ import { IUser } from "../../models/user.interface";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const getCart = async (req: Request, res: Response, _: NextFunction) => {
-  const user = req.user as IUser;
+	const user = req.user as IUser;
 
-  const userId = user._id;
+	const userId = user._id;
 
-  const cart = await cartModel.find({ userId });
+	const cart = await cartModel.find({ user: userId });
 
-  if (!cart || cart === null || cart.length === 0) {
-    return res.status(404).json({
-      success: true,
-      message: "User doesn't have a cart yet",
-    });
-  }
+	if (!cart || cart === null || cart.length === 0) {
+		return res.status(404).json({
+			success: true,
+			message: "User doesn't have a cart yet",
+		});
+	}
 
-  res.status(200).json({
-    success: true,
-    message: "Fetched user cart successfully",
-    payload: { cart },
-  });
+	res.status(200).json({
+		success: true,
+		message: "Fetched user cart successfully",
+		payload: { cart },
+	});
 };
 
 export { getCart };
