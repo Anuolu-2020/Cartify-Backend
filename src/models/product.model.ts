38b3c05e7051c2218386e6d20435b7bf0fabb7c9
Anuolu-2020 <Anuolu1645@gmail.com>
2024-08-17 00:18:40 +0100

    chore: changed vendorId to vendor and changed quantity to units

--- src/models/product.model.ts
+++ src/models/product.model.ts
@@ -5,52 +5,49 @@ import reviewModel from "./review.model";
 const Schema = mongoose.Schema;
 
 const productSchema = new Schema<IProducts>({
-  vendorId: {
-    type: mongoose.Types.ObjectId,
-    ref: "Users",
-    required: [true, "Vendor id not provided"],
-  },
-  name: {
-    type: String,
-    required: [true, "A product name must be provided"],
-  },
-  photo: {
-    type: String,
-    required: [true, "Product photo url must be provided"],
-  },
-  productDetails: {
-    type: String,
-    required: [true, "Product details muust be provided"],
-  },
-  category: {
-    type: String,
-    required: [true, "A category must be provided for the product"],
-  },
-  price: {
-    type: Number,
-    default: 0,
-    required: [true, "A price must be provided for the product"],
-    min: 0,
-  },
-  vendorName: {
-    type: String,
-    required: [true, "A vendor name must be provided for the product"],
-  },
-  vendorAddress: {
-    type: String,
-    required: [true, "Vendor address must be provided"],
-  },
+	vendor: {
+		type: mongoose.Types.ObjectId,
+		ref: "Users",
+		required: [true, "Vendor id not provided"],
+	},
+	name: {
+		type: String,
+		required: [true, "A product name must be provided"],
+	},
+	photo: {
+		type: String,
+		required: [true, "Product photo url must be provided"],
+	},
+	productDetails: {
+		type: String,
+		required: [true, "Product details muust be provided"],
+	},
+	category: {
+		type: String,
+		required: [true, "A category must be provided for the product"],
+	},
+	price: {
+		type: Number,
+		default: 0,
+		required: [true, "A price must be provided for the product"],
+		min: 0,
+	},
+	units: {
+		type: Number,
+		required: [true, "A stock amount must be provided for the product"],
+		min: 0,
+	},
 });
 
-productSchema.virtual("averageRating").get(async function(this: IProducts) {
-  const productId = this._id;
+productSchema.virtual("averageRating").get(async function (this: IProducts) {
+	const productId = this._id;
 
-  const averageRating = await reviewModel.aggregate([
-    { $match: { productId } },
-    { $group: { _id: "$productId", averageRating: { $avg: "$rating" } } },
-  ]);
+	const averageRating = await reviewModel.aggregate([
+		{ $match: { productId } },
+		{ $group: { _id: "$productId", averageRating: { $avg: "$rating" } } },
+	]);
 
-  return averageRating.length > 0 ? averageRating[0]?.averageRating : 0;
+	return averageRating.length > 0 ? averageRating[0]?.averageRating : 0;
 });
 
 const productModel = mongoose.model<IProducts>("Products", productSchema);
