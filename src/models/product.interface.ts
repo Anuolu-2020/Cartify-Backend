94ab9e6e9dce9033feb6746c83bce87baaa3d77d
Anuolu-2020 <Anuolu1645@gmail.com>
2024-08-17 00:18:17 +0100

    chore: changed vendorId to vendor

--- src/models/product.interface.ts
+++ src/models/product.interface.ts
@@ -1,16 +1,17 @@
 import mongoose, { Document } from "mongoose";
 
 export interface IProducts extends Document {
-  vendorId: {
-    ref: string;
-    type: mongoose.Types.ObjectId;
-  };
-  name: string;
-  photo: string;
-  productDetails: string;
-  category: string;
-  price: number;
-  rating: number;
-  vendorName: string;
-  vendorAddress: string;
+	vendor: {
+		ref: string;
+		type: mongoose.Types.ObjectId;
+	};
+	name: string;
+	photo: string;
+	productDetails: string;
+	category: string;
+	price: number;
+	rating: number;
+	vendorName: string;
+	vendorAddress: string;
+	units: number;
 }
