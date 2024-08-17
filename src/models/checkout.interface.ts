b52537031ee44e5be5550ec0d1f069659fd1127c
Anuolu-2020 <Anuolu1645@gmail.com>
2024-08-18 00:54:06 +0100

    feat: added discount to checkout interface

---
+++ src/models/checkout.interface.ts
@@ -0,0 +1,20 @@
+import { Document, ObjectId } from "mongoose";
+
+interface Product {
+	vendor: ObjectId;
+	productId: ObjectId;
+	name: string;
+	photo: string;
+	category: string;
+	price: number;
+	units: number;
+}
+
+export interface ICheckout extends Document {
+	userId: ObjectId;
+	products: Product[];
+	totalPrice: number;
+	totalDiscountedPrice: number;
+	shippingAddress: string;
+	deliveryFee: number;
+}
