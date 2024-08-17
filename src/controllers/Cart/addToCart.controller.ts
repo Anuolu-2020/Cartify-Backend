e64cb0a2905ebae16e314c046f2c22e7587c7694
Anuolu-2020 <Anuolu1645@gmail.com>
2024-08-18 00:51:14 +0100

    feat: added discounts to cart products

--- src/controllers/Cart/addToCart.controller.ts
+++ src/controllers/Cart/addToCart.controller.ts
@@ -17,7 +17,7 @@ const addToCart = async (req: Request, res: Response, next: NextFunction) => {
 	}
 
 	try {
-		const { productId, quantity } = req.body;
+		const { productId, units } = req.body;
 
 		//Get user from request object
 		const user = req.user as IUser;
@@ -35,12 +35,12 @@ const addToCart = async (req: Request, res: Response, next: NextFunction) => {
 			return next(new errorHandler(403, "Product out of stock"));
 		}
 
-		if (quantity > product.units) {
+		if (units > product.units) {
 			return next(new errorHandler(403, "Not enough product stock available"));
 		}
 
 		//Calculate product total price
-		const productPrice = product.price * quantity;
+		const productPrice = product.price * units;
 
 		//Check if user already has a cart
 		let cart = await CartModel.findOne({ user: userId });
@@ -56,7 +56,7 @@ const addToCart = async (req: Request, res: Response, next: NextFunction) => {
 			if (itemIndex > -1) {
 				const productItem = cart.products[itemIndex];
 
-				productItem.quantity = quantity;
+				productItem.units = units;
 
 				productItem.price = productPrice;
 
@@ -64,19 +64,32 @@ const addToCart = async (req: Request, res: Response, next: NextFunction) => {
 				cart.totalPrice = cart.products
 					.map((item) => item.price)
 					.reduce((acc, next) => acc + next);
+
+				// Get total discounted pice
+				cart.totalDiscountedPrice = cart.products.reduce((acc, product) => {
+					return acc + product.discountedPrice * product.units;
+				}, 0);
 			} else {
 				//Add new product to cart
 				cart.products.push({
 					productId,
 					name: product.name,
+					photo: product.photo,
 					price: productPrice,
-					quantity,
+					units,
+					discountPercentage: product.discountPercentage,
+					discountedPrice: product.discountPrice,
 				});
 
 				//Get total price of all products
 				cart.totalPrice = cart.products
 					.map((item) => item.price)
 					.reduce((acc, next) => acc + next);
+
+				// Get total discounted pice
+				cart.totalDiscountedPrice = cart.products.reduce((acc, product) => {
+					return acc + product.discountedPrice * product.units;
+				}, 0);
 			}
 
 			//Save the cart
@@ -84,13 +97,29 @@ const addToCart = async (req: Request, res: Response, next: NextFunction) => {
 
 			res.status(200).json(cart);
 		} else {
+			// Calculate discount price
+			const discountedPrice =
+				product.price * (1 - product.discountPercentage / 100);
+
+			//Calculate total discount price
+			const totalDiscountedPrice = discountedPrice * product.units;
+
 			//User doesn't have a cart, Create a new cart
 			const newCart = await new CartModel({
 				user: userId,
 				products: [
-					{ productId, name: product.name, price: productPrice, quantity },
+					{
+						productId,
+						name: product.name,
+						price: productPrice,
+						photo: product.photo,
+						units,
+						discountPercentage: product.discountPercentage,
+						discountedPrice,
+					},
 				],
 				totalPrice: productPrice,
+				totalDiscountedPrice,
 			}).save();
 
 			res.status(201).json(newCart);
