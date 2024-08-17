312baa23e6769adf61d1298c06541898311830af
Anuolu-2020 <Anuolu1645@gmail.com>
2024-08-18 00:56:35 +0100

    chore: minor change

--- src/models/user.interface.ts
+++ src/models/user.interface.ts
@@ -1,16 +1,14 @@
-//import { Request } from "express";
 import { Document } from "mongoose";
 
 export interface IUser extends Document {
-  //_id: string;
-  fullname: string;
-  email: string;
-  password: string;
-  passwordConfirm: string;
-  username: string;
-  role: string;
-  address: string;
-  phoneNumber: string;
-  createdAt: Date;
-  updatedAt: Date;
+	fullname: string;
+	email: string;
+	password: string;
+	passwordConfirm: string;
+	username: string;
+	role: string;
+	address: string;
+	phoneNumber: string;
+	createdAt: Date;
+	updatedAt: Date;
 }
