d25df4de928252ecb0e6d8a132033da5f26b922d
Anuolu-2020 <Anuolu1645@gmail.com>
2024-08-18 00:54:48 +0100

    chore: add discount to order interface

---
+++ src/models/order.interface.ts
@@ -0,0 +1,25 @@
+import { Document, ObjectId } from "mongoose";
+
+interface Product {
+	vendor: ObjectId;
+	productId: ObjectId;
+	name: string;
+	photo: string;
+	category: string;
+	price: number;
+	units: number;
+}
+
+export interface IOrder extends Document {
+	userId: ObjectId;
+	products: Product[];
+	totalPrice: number;
+	paymentMethod: string;
+	paymentStatus: "completed" | "pending" | "failed";
+	shippingAddress: string;
+	orderDate: Date;
+	deliveryDate?: Date;
+	orderStatus: "pending" | "shipped" | "delivered" | "canceled";
+	trackingNumber?: string;
+	discount?: number;
+}
