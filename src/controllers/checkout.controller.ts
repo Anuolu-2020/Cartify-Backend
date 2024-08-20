d579e47557eb703051ef861a499cc8c12b70436b
Anuolu-2020 <Anuolu1645@gmail.com>
2024-08-20 23:46:45 +0100

    feat: checkout controller completed

--- src/controllers/checkout.controller.ts
+++ src/controllers/checkout.controller.ts
@@ -39,6 +39,7 @@ export const checkout = async (
 	//Hard coded for now
 	const deliveryFee = 600;
 
+	// calculate total price after discount, deliveryFee and possibly tax
 	const grandTotal = cart.totalPrice - cart.totalDiscountedPrice + deliveryFee;
 
 	//If theres already a checkout
@@ -47,10 +48,12 @@ export const checkout = async (
 		const updatedCheckout = await CheckoutModel.findOneAndUpdate(
 			{ userId: userId },
 			{
+				email: userFound.email,
 				products: cart.products,
 				totalPrice: cart.totalPrice,
 				shippingAddress: userFound.address,
 				totalDiscountedPrice: cart.totalDiscountedPrice,
+				grandTotal,
 			},
 			{ new: true, runValidators: true },
 		);
@@ -63,14 +66,15 @@ export const checkout = async (
 		return sendResponse(res, 200, "Checkout data fetched successfully", data);
 	}
 
-	
 	const checkout = await CheckoutModel.create({
 		userId,
+		email: userFound.email,
 		products: cart.products,
 		totalPrice: cart.totalPrice,
 		totalDiscountedPrice: cart.totalDiscountedPrice,
 		shippingAddress: userFound.address,
 		deliveryFee,
+		grandTotal,
 	});
 
 	const data = {
