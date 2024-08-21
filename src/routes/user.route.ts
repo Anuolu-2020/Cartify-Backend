528c02d6deeb8e33dd88157f22ea364e11faad54
Anuolu-2020 <Anuolu1645@gmail.com>
2024-08-21 23:17:28 +0100

    chore: minor change

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -12,12 +12,10 @@ import {
 } from "../controllers/user.controller";
 import { checkout } from "../controllers/checkout.controller";
 import { createOrder } from "../controllers/Order/order.controller";
-import { paymentWebhook } from "../controllers/payment.webhook";
 
 userRoute.route("/update-role").put(updateRole);
 userRoute.route("/checkout").get(checkout);
 userRoute.route("/order").post(createOrder);
-userRoute.route("/order/transaction/webhook").post(paymentWebhook);
 
 userRoute.use(isRestrictedTo("admin"));
 
