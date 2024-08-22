46d4a3b438d587602224c0eb69f51d27f11dab51
Anuolu-2020 <Anuolu1645@gmail.com>
2024-08-22 20:20:49 +0100

    feat: add get and accept order field route

--- src/routes/Products/vendor.route.ts
+++ src/routes/Products/vendor.route.ts
@@ -9,6 +9,8 @@ import { uploadProduct } from "../../controllers/Products/uploadProduct.controll
 import { getVendorProducts } from "../../controllers/Products/getProducts.controller";
 import deleteVendorProducts from "../../controllers/Products/deleteProducts";
 import { isRestrictedTo } from "../../middlewares/roleAuth.middleware";
+import { getVendorOrders } from "../../controllers/vendor/getOrders.controller";
+import { acceptOrder } from "../../controllers/vendor/acceptOrder.controller";
 
 // Setting up multer as a middleware to grab photo uploads
 const storage = multer.memoryStorage();
@@ -18,10 +20,10 @@ const upload = multer({ storage: storage });
 
 router.use(isRestrictedTo("vendor", "admin"));
 
-router
-  .route("/product")
-  .post(upload.single("productImage"), uploadProduct);
+router.route("/product").post(upload.single("productImage"), uploadProduct);
 
 router.route("/products").get(getVendorProducts).delete(deleteVendorProducts);
+router.route("/products/orders").get(getVendorOrders);
+router.route("/products/orders/:orderId").post(acceptOrder);
 
 export { router };
