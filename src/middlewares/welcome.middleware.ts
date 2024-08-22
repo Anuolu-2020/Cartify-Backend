44f736513f8475eafbcc2bcd27135c6f69df03ad
Anuolu-2020 <Anuolu1645@gmail.com>
2024-08-22 22:27:01 +0100

    chore: minor change

--- src/middlewares/welcome.middleware.ts
+++ src/middlewares/welcome.middleware.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 
 export const welcomeToApi = (req: Request, res: Response) => {
 	const { version } = req.params;
+
 	res.status(200).json({
 		status: "success",
 		message: `welcome to Afrique spark Api v${version}`,
