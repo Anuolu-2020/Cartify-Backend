e6de64c99d190d0d494ee07ce4e9f6329030e5b3
Anuolu-2020 <Anuolu1645@gmail.com>
2024-10-01 00:31:06 +0100

    feat: uploading of multiple images for a product now implemented

--- src/controllers/Products/uploadProduct.controller.ts
+++ src/controllers/Products/uploadProduct.controller.ts
@@ -1,72 +1,76 @@
+import { NextFunction, Request, Response } from "express";
+import {
+	getDownloadURL,
+	ref,
+	updateMetadata,
+	uploadBytesResumable,
+} from "firebase/storage";
+import xhr2 from "xhr2";
 import bucketStorage from "../../config/firebase.config";
 import productModel from "../../models/product.model";
-import { validateProductUpload } from "../../utils/validateUserInput";
-import {
-	ref,
-	getDownloadURL,
-	updateMetadata,
-	uploadBytes,
-} from "firebase/storage";
-import { Request, Response, NextFunction } from "express";
-
-// Workaround for the bug in xhr2 with firebase
-import xhr2 from "xhr2"; // must be used to avoid bug
-
 import { IUser } from "../../models/user.interface";
+import { validateProductUpload } from "../../utils/validateUserInput";
 
 global.XMLHttpRequest = xhr2;
 
-// Upload a product
-const uploadProduct = async (
-	req: Request,
-	res: Response,
-	next: NextFunction,
-) => {
+async function uploadFilesToFirebase(
+	file: Express.Multer.File,
+): Promise<string> {
+	const timestamp = Date.now();
+	const name = file.originalname.split(".")[0];
+	const type = file.originalname.split(".")[1];
+	const fileName = `${name}_${timestamp}.${type}`;
+
+	// Step 1. Create reference for storage and file name in cloud
+	const imageRef = ref(bucketStorage, `images/${fileName}`);
+
 	try {
-		// Validate the product upload
-		const { error } = validateProductUpload(req.body);
-
-		if (error) {
-			const errorMessage = error.details[0].message.replace(/"/g, ""); // strip out quotes
-			return res.status(400).json({
-				success: false,
-				message: errorMessage,
-			});
-		}
-
-		//Grab file
-		const file = req.file;
-
-		if (!file) {
-			return res.status(400).json({
-				success: false,
-				message: "No image provided",
-			});
-		}
-
-		// Format the filename
-		const timestamp = Date.now();
-		const name = file.originalname.split(".")[0];
-		const type = file.originalname.split(".")[1];
-		const fileName = `${name}_${timestamp}.${type}`;
-
-		// Step 1. Create reference for storage and file name in cloud storage
-		const imageRef = ref(bucketStorage, `images/${fileName}`);
-
 		// Step 2. Upload the file in the bucket storage
-		const uploadImage = await uploadBytes(imageRef, file.buffer);
+		const uploadImage = await uploadBytesResumable(imageRef, file.buffer);
 
 		// Create file metadata.
 		const newMetadata = {
-			cacheControl: "public,max-age=2629800000", // 1 month
+			cacheControl: "public,max-age=2629800000",
 			contentType: uploadImage.metadata.contentType,
 		};
 
 		// Update the metadata for the file.
 		await updateMetadata(imageRef, newMetadata);
 
-		// Get the image URL.
-		const downloadURL = await getDownloadURL(imageRef);
+		//return image URL.
+		return await getDownloadURL(imageRef);
+	} catch (err) {
+		throw new Error(`An Error occurred while uploading: ${err}`);
+	}
+}
+
+const uploadProduct = async (
+	req: Request,
+	res: Response,
+	next: NextFunction,
+) => {
+	try {
+		const { error } = validateProductUpload(req.body);
+		if (error) {
+			const errorMessage = error.details[0].message.replace(/"/g, "");
+			return res.status(400).json({
+				success: false,
+				message: errorMessage,
+			});
+		}
+
+		const files = req.files as Express.Multer.File[];
+		if (!files || files.length === 0) {
+			return res.status(400).json({
+				success: false,
+				message: "No image provided",
+			});
+		}
+
+		//Upload images and wait for all image url
+		const uploadedImagesUrls = await Promise.all(
+			files.map(uploadFilesToFirebase),
+		);
 
 		const {
 			productName,
@@ -78,14 +82,12 @@ const uploadProduct = async (
 		} = req.body;
 
 		const vendor = req.user as IUser;
-
 		const vendorId = vendor._id;
 
-		// Save the product to the database
 		const newProduct = await new productModel({
 			vendor: vendorId,
 			name: productName,
-			photo: downloadURL,
+			photo: uploadedImagesUrls,
 			productDetails: productDetails,
 			price: productPrice,
 			category: category,
