e35bb4eeab60515c2ad9e2341b664ef4733e4072
Anuolu-2020 <Anuolu1645@gmail.com>
2024-10-16 10:41:07 +0100

    chore: minor change

--- src/utils/ApiFeatures.ts
+++ src/utils/ApiFeatures.ts
@@ -2,38 +2,38 @@ import { Document, Query } from "mongoose";
 import { ReqQuery } from "../types/requestQuery.interface";
 
 class ApiFeatures<T extends Document> {
-  public modelQuery: Query<T[], Document<unknown, unknown, T>, object>;
-  public query: ReqQuery;
+	public modelQuery: Query<T[], Document<unknown, unknown, T>, object>;
+	public query: ReqQuery;
 
-  constructor(
-    modelQuery: Query<T[], Document<unknown, unknown, T>, object>,
-    query: ReqQuery,
-  ) {
-    this.modelQuery = modelQuery;
+	constructor(
+		modelQuery: Query<T[], Document<unknown, unknown, T>, object>,
+		query: ReqQuery,
+	) {
+		this.modelQuery = modelQuery;
 
-    this.query = query;
-  }
+		this.query = query;
+	}
 
-  public limitFields() {
-    if (this.query.fields) {
-      const fields = this.query.fields.split(",").join(" ");
-      this.modelQuery = this.modelQuery.select(fields);
-    } else {
-      this.modelQuery = this.modelQuery.select("-__v");
-    }
+	public limitFields() {
+		if (this.query.fields) {
+			const fields = this.query.fields.split(",").join(" ");
+			this.modelQuery = this.modelQuery.select(fields);
+		} else {
+			this.modelQuery = this.modelQuery.select("-__v");
+		}
 
-    return this;
-  }
+		return this;
+	}
 
-  public paginate() {
-    const page = parseInt(this.query.page) * 1 || 1;
-    const limit = parseInt(this.query.limit) * 1 || 100;
-    const skip = (page - 1) * limit;
+	public paginate() {
+		const page = parseInt(this.query.page) * 1 || 1;
+		const limit = parseInt(this.query.limit) * 1 || 10;
+		const skip = (page - 1) * limit;
 
-    this.modelQuery = this.modelQuery.skip(skip).limit(limit);
+		this.modelQuery = this.modelQuery.skip(skip).limit(limit);
 
-    return this;
-  }
+		return this;
+	}
 }
 
 export { ApiFeatures };
